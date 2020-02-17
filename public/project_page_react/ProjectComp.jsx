01ec1ded4256bb98b21aa6af3d93585cc21a3e37
Amaka Anikwe <amakaanikwe@gmail.com>
2020-02-17 18:26:01 -0500

    Update ProjectComp.jsx

---
+++ public/project_page_react/ProjectComp.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ProjectComponent extends React.Component{
+  constructor(props) {
+    super(props);
+     
+      }
+
+  }
+  render () {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+export default ProjectComponent;
