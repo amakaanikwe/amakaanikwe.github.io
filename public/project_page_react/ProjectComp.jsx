acf6fd57bd78eeb0c397d8b8cc52bbc9cc326970
Amaka Anikwe <amakaanikwe@gmail.com>
2020-02-17 18:46:20 -0500

    Update

--- public/project_page_react/ProjectComp.jsx
+++ public/project_page_react/ProjectComp.jsx
@@ -6,13 +6,29 @@ class ProjectComponent extends React.Component{
      
       }
 
-  }
+  
   render () {
     return (
       <React.Fragment>
+          <section>
+              <div className="row">
+                  <div className="col-lg-5">
+                    <a href={}><img
+                    class="img-responsive"
+                    src={}
+                    width="400"
+                    /></a>
+                  </div>
+                  <div className="col-lg-7">
+                    <h2>{}</h2>
+                    <footer></footer>
+                  </div>
+              </div>
+          </section>
 
       </React.Fragment>
     );
   }
 }
+
 export default ProjectComponent;
