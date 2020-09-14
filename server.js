6292465a217ab5f7c4722cd36e574a15d157e059
Amaka Anikwe <amakaanikwe@gmail.com>
2020-09-14 06:29:32 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const sedMail = require("./mail.js")
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const exhbs = require("express-handlebars");
 const nodemailer = require('nodemailer');
 const path = require("path");
+const sendMail = require("./mail.js");
 const log = console.log;
 
 const app = express();
@@ -24,36 +26,49 @@ app.get("/", (req, res) => {
 
 //POST route from contact form
 app.post('/email', function (req, res) {
+
   console.log("Data: ", req.body);
-  res.json({message: "Message received"})
 
-  let mailOpts, smtpTrans;
-  smtpTrans = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.GMAIL_USER,
-      pass: process.env.GMAIL_PASS
-    }
-  });
-  mailOpts = {
-    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
-    to: process.env.GMAIL_USER,
-    subject: 'New message from contact form',
-    text: `${req.body.name} (${req.body.email}) says: 
-    ${req.body.message}`
-  };
+  const{name, email, text} = req.body;
   
-  smtpTrans.sendMail(mailOpts, function(error, res) { 
-    if (error) { 
-          res.sendFile('contact-failure');
-        }
-        else {
-          res.sendFile('contact-success');
-        }
+  sendMail(email, name, text, function(err, res) { 
+      if (err) { 
+            res.status(500).json({message: "Internal Error"});
+          }
+          else {
+            res.json({message: "message received!!!"});
+          }
+        });
 
-  });
+
+  // let mailOpts, smtpTrans;
+  // smtpTrans = nodemailer.createTransport({
+  //   host: 'smtp.gmail.com',
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     user: process.env.GMAIL_USER,
+  //     pass: process.env.GMAIL_PASS
+  //   }
+  // });
+
+  // mailOpts = {
+  //   from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+  //   to: process.env.GMAIL_USER,
+  //   subject: 'New message from contact form',
+  //   text: `${req.body.name} (${req.body.email}) says: 
+  //   ${req.body.message}`
+  // };
+  
+  // smtpTrans.sendMail(mailOpts, function(error, res) { 
+  //   if (error) { 
+  //         res.sendFile('contact-failure');
+  //       }
+  //       else {
+  //         res.sendFile('contact-success');
+  //       }
+
+  // });
 
 
 });
