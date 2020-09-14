c67fe903f155187111d5e2760b46cfec869beeb9
Amaka Anikwe <amakaanikwe@gmail.com>
2020-09-14 07:27:24 -0400

    Updated gitignore

--- mail.js
+++ mail.js
@@ -1,10 +1,11 @@
 const nodemailer = require("nodemailer");
 const mailGun= require("nodemailer-mailgun-transport");
+require('dotenv').config();
 
 const auth = {
     auth:{
-        api_key: "129ec3666e48c1fcc8226f36a25fb3f2-e566273b-a759ea8c",
-        domain: "sandbox77b9ec6d806b4664b4cf1762b7e73769.mailgun.org"
+        api_key: process.env.API_KEY,
+        domain: process.env.DOMAIN
     }
 };
 
